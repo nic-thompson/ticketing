ba08567bb13f8cec090e914953ec458ed58cfe44
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-10 10:32:25 +0100

    Sets up ingress service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000 with skaffold!');
 });
