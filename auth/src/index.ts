3e16dd3a89b0f2abe21c6565dd65957f7cc3a990
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-11 11:05:31 +0100

    Sets up and connects to a mongoDB deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -21,6 +22,16 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000 with skaffold!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000 with skaffold!');
+  });
+};
+
+start();
