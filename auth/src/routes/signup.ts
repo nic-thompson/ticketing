2984801eea05a9d222f08bbba1d01d30a93da62b
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-11 12:20:19 +0100

    Check the database for an existing user

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -16,13 +16,24 @@ router.post(
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+
     const { email, password } = req.body;
-    console.log('Creating a new user...');
-    throw new DatabaseConnectionError();
-    res.send({});
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
