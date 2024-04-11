8fa78ad8643d0e76ad57f25a94eb768379007eba
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-11 11:45:46 +0100

    Adds user schema

---
+++ auth/src/models/user.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+export { User };
