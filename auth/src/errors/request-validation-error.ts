1fa274038fff7d65852aa947f692313c729ec589
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-11 09:55:24 +0100

    Handles errors with an abstract custom error class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,10 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid request parameters');
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
@@ -12,6 +13,7 @@ export class RequestValidationError extends Error {
       if (error.type === 'field') {
         return { message: error.msg, field: error.path };
       }
+      return { message: error.msg };
     });
   }
 }
