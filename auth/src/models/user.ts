d3f0b6dbdb599594b2fff8ae49e6a4d18ef08b77
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-11 11:58:20 +0100

    Adds user document interface

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -5,8 +5,13 @@ interface UserAttrs {
   password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -25,6 +30,11 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+const user = User.build({
+  email: 'XXXXXXXXXXXXX',
+  password: 'XXXXXXXXXXXXX',
+});
 
 export { User };
