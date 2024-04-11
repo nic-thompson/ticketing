3f9ec3882ed8c0de220249c94365945efe6db55e
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-11 13:37:06 +0100

    Hashes password on save

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 interface UserAttrs {
   email: string;
@@ -26,15 +27,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
-
-const user = User.build({
-  email: 'XXXXXXXXXXXXX',
-  password: 'XXXXXXXXXXXXX',
-});
 
 export { User };
