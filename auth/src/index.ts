029ac2a3fb51caf89d99625e7affeb2575435fb9
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-11 21:14:10 +0100

    Sets JWT in session cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -9,7 +11,9 @@ import { errorHandler } from './middlewares/error-handlers';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(cookieSession({ signed: false, secure: true }));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
