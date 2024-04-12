afa5a500d66eba07a954c08284857dfed26a0fa3
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-13 00:30:47 +0100

    Tests invalid inputs

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,30 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'notanemail',
+      password: 'XXXXXX',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@nicolasthompson.com',
+      password: 'X',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({}) // empty object
+    .expect(400);
+});
