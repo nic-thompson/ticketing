b1376fde0164a3c1927d3b0b4d30d0bc7f9c64a4
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-12 16:35:32 +0100

    Creates require auth middleware

---
+++ auth/src/middlewares/require-auth.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorisedError } from '../errors/not-authorised-error';
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.currentUser) {
+    res.status(401);
+    throw new NotAuthorisedError();
+  }
+
+  next();
+};
