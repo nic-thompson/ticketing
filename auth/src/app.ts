a3e9b352d16540865b709e5f04f74e46a5213931
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-13 00:54:01 +0100

    Set secure cookie session to false in test environment

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -12,7 +12,9 @@ import { NotFoundError } from './errors/not-found-error';
 const app = express();
 app.set('trust proxy', true);
 app.use(express.json());
-app.use(cookieSession({ signed: false, secure: true }));
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
