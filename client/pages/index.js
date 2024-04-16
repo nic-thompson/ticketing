d0d6cac066a7db0b328d998542de53afec75423e
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-16 13:05:46 +0100

    Creates a reusable API client

---
+++ client/pages/index.js
@@ -0,0 +1,13 @@
+import buildClient from '../api/build-client';
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
