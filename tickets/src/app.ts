52b7fc4879ad5e31a2329f2e6550cd5770594df0
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-17 12:10:15 +0100

    Sets up tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@nicolas.thompson/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
+);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
