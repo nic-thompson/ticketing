5c0e5a16732345cdd2e4bbfc338291c7c3cde6ec
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-17 14:10:36 +0100

    Adds requireAuth to new ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@nicolas.thompson/common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@nicolas.thompson/common';
 import { createTicketRouter } from './routes/new';
 
 const app = express();
@@ -10,6 +14,8 @@ app.use(express.json());
 app.use(
   cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
 );
+
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -6,7 +6,9 @@ it('has a route handler listening to /api/tickets for post requests', async () =
 
   expect(response.status).not.toEqual(404);
 });
-it('can only be accessed if the user is signed in', async () => {});
+it('can only be accessed if the user is signed in', async () => {
+  await request(app).post('/api/tickets').send({}).expect(401);
+});
 it('returns an error if an invalid title is provided', async () => {});
 it('returns an error if an invalid price is provided', async () => {});
 it('creates a ticket with valid inputs', async () => {});
