290f82f9781f27e78a674e3040d46ed3e931ee55
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-17 16:59:10 +0100

    Tests it returns a 404 if the ticket is not found

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import {
   currentUser,
 } from '@nicolas.thompson/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
