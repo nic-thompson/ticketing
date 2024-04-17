273d8a809062f29f1acb19a7d43a21d311724e3e
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-17 18:25:59 +0100

    Tests it can fetch a list of tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import {
 } from '@nicolas.thompson/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
