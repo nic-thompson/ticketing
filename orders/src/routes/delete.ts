72db7457ea8ea89437b8efc92b79458225db8fb1
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-22 18:11:27 +0100

    Tests it marks an order as cancelled

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,31 @@
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorisedError,
+  NotFoundError,
+  requireAuth,
+} from '@nicolas.thompson/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId).populate('ticket');
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorisedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    // Publish an event saying this was cancelled!
+
+    res.sendStatus(204);
+  }
+);
 
 export { router as deleteOrderRouter };
