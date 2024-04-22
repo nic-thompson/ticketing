a27c31bf9d167ccd197c030f5a10f19e4f06d929
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-22 14:46:09 +0100

    Tests creates new order

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,51 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
+
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  const mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+}, 10000);
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  return [
+    `session=${Buffer.from(
+      JSON.stringify({
+        jwt: jwt.sign(
+          {
+            id: new mongoose.Types.ObjectId().toHexString(),
+            email: 'test@test.com',
+          },
+          process.env.JWT_KEY!
+        ),
+      })
+    ).toString('base64')}`,
+  ];
+};
