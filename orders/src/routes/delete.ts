6f722cac8e4d5a5031e7e29e8c429a53a0d7505d
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-26 16:42:17 +0100

    Adds versioning to ticket created and ticket updated events

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -28,6 +28,7 @@ router.delete(
     // Publish an event saying this was cancelled
     new OrderCancelledPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.version,
       ticket: {
         id: order.ticket.id,
       },
