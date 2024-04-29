5e3b3324fe6dc2c0c5c1ec0712c45cb7cb6af216
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-29 13:22:46 +0100

    Payment service set up

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@nicolas.thompson/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
+);
+
+app.use(currentUser);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
