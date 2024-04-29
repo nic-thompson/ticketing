2fc2947937da48d72a98169055229696a64575a2
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-29 12:03:06 +0100

    Tests expiration complete listener

---
+++ orders/src/events/listeners/__tests__/expiration-complete-listener.test.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import { ExpirationCompleteListener } from '../expiration-complete-listener';
+import { OrderStatus, ExpirationCompleteEvent } from '@nicolas.thompson/common';
+import { Message } from 'node-nats-streaming';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Order } from '../../../models/order';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: 'alskdfj',
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent['data'] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, order, data, msg };
+};
+
+it('updates the order status to cancelled', async () => {
+  const { listener, order, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emits an OrderCancelled event', async () => {
+  const { listener, order, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const eventData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(eventData.id).toEqual(order.id);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
