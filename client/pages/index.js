1eed60049e97180949572d87ff62b6d525fc433e
Nicolas Thompson <mail@nicolasthompson.com>
2024-05-01 16:27:59 +0100

    Passes client and current user with getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,3 @@
-import buildClient from '../api/build-client';
 const LandingPage = ({ currentUser }) => {
   return currentUser ? (
     <h1>You are signed in.</h1>
@@ -8,9 +7,7 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
+  return {};
 };
 
 export default LandingPage;
