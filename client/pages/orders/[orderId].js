af1048eab37942c702c4bf4ab856f43e8c9aec84
Nicolas Thompson <mail@nicolasthompson.com>
2024-05-02 11:48:27 +0100

    Shows the order expiration timer

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,9 +1,36 @@
-const OrderShow = () => {
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
   return (
     <div>
-      <h1>Order Show</h1>
+      <h1>Time left to pay: {timeLeft} seconds</h1>
     </div>
   );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+  return { order: data };
 };
 
 export default OrderShow;
