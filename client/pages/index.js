605347c425a7d24430d522fde6404fe29816d531
Nicolas Thompson <mail@nicolasthompson.com>
2024-05-02 08:01:17 +0100

    Lists all tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,30 @@
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in.</h1>
-  ) : (
-    <h1>You are NOT signed in.</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => (
+    <tr key={ticket.id}>
+      <td>{ticket.title}</td>
+      <td>{ticket.price}</td>
+    </tr>
+  ));
+
+  return (
+    <div>
+      <h2>Tickets</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-LandingPage.getInitialProps = async (context) => {
-  return {};
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+  return { tickets: data };
 };
 
 export default LandingPage;
